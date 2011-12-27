1d0d5f891163473d54db057bb61a314605702af5
Stephen Thirlwall <sdt@dr.com>
2011-12-27 14:30:14 +1100

    Consolidate and document class builder

--- lib/MC/ClassBuilder.pm
+++ lib/MC/ClassBuilder.pm
@@ -6,44 +6,20 @@ use warnings;
 
 use Moose ();
 
+my $head_method_wrapper = sub {
+    my ($orig, $self, @args) = @_;
+    $self->$orig($self, @args); # 2nd $self is $head param
+};
+
 sub import {
     my ($class, $name, @methods) = @_;
 
-    _create_tail_role($name, @methods);
-    _create_body_role($name, @methods);
-    _create_head_class($name, @methods);
-}
-
-sub _create_tail_role {
-    my ($name, @methods) = @_;
-
-    # Effectively does this:
-    #   package ${name}::Role::Tail;
-    #   use Moose::Role;
-    #   requires @methods;
-
     my $tailname = $name . '::Role::Tail';
-    my $role = Moose::Meta::Role->create($tailname);
-    $role->add_required_methods(@methods);
-}
-
-sub _create_body_role {
-    my ($name, @methods) = @_;
-
-    # Effectively does this:
-    #   package ${name}::Role::Body;
-    #   use Moose::Role;
-    #   with '${name}::Role::Tail';
-    #   has tail => (
-    #       is       => 'ro',
-    #       does     => '${name}::Role::Tail',
-    #       required => 1,
-    #   );
+    my $tailrole = Moose::Meta::Role->create($tailname);
+    $tailrole->add_required_methods(@methods);
 
     my $bodyname = $name . '::Role::Body';
-    my $tailname = $name . '::Role::Tail';
-
-    my $role = Moose::Meta::Role->create($bodyname,
+    my $bodyrole = Moose::Meta::Role->create($bodyname,
             roles => [ $tailname ],
             attributes => {
                 tail => {
@@ -53,34 +29,9 @@ sub _create_body_role {
                 },
             },
         );
-}
-
-sub _create_head_class {
-    my ($name, @methods) = @_;
-
-    # Effectively does this:
-    #   package ${name}::Head;
-    #   use Moose;
-    #   has body => (
-    #       is          => 'ro',
-    #       does        => '${name}::Role::Tail',
-    #       required    => 1,
-    #       handles     => [qw( $methods )],
-    #   );
-    #   around [qw( $methods )] => sub {
-    #       my ($orig, $self, @args) = @_;
-    #       $self->$orig($self, @args);
-    #   };
 
     my $headname = $name . '::Head';
-    my $tailname = $name . '::Role::Tail';
-
-    my $method_wrapper = sub {
-            my ($orig, $self, @args) = @_;
-            $self->$orig($self, @args); # 2nd $self is $head param
-        };
-
-    my $class = Moose::Meta::Class->create($headname,
+    my $headclass = Moose::Meta::Class->create($headname,
             attributes => [
                 Moose::Meta::Attribute->new(
                     body => (
@@ -94,9 +45,54 @@ sub _create_head_class {
             superclasses => [qw( Moose::Object )],
         );
     for my $method (@methods) {
-        $class->add_around_method_modifier($method, $method_wrapper);
+        $headclass->add_around_method_modifier($method, $head_method_wrapper);
     }
-    $class->make_immutable;
+    $headclass->make_immutable;
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    package MyCentipede;
+    use MC::ClassBuilder MyCentipede => qw( get set );
+
+    # Will automatically create the following roles and classes:
+
+    # Tail role - consuming classes must implement the interface.
+    package MyCentipede::Role::Tail;
+    use Moose::Role;
+    requires qw( get set );
+
+    # Body role - consuming classes must implement the interface, and also
+    # have a tail attribute.
+    package MyCentipede::Role::Body;
+    use Moose::Role;
+    with 'MyCentipede::Role::Tail';
+    has tail => (
+        is       => 'ro',
+        does     => 'MyCentipede::Role::Tail',
+        required => 1,
+    );
+
+    # Head class. This is just a convenience class which automatically wraps
+    # the interface methods so they provide the $head parameter.
+    package MyCentipede::Head;
+    use Moose;
+    has body => (
+        is          => 'ro',
+        does        => 'MyCentipede::Role::Tail',
+        required    => 1,
+        handles     => [qw( get set )],
+    );
+    around [qw( get set )] => sub {
+        my ($orig, $self, @args) = @_;
+        $self->$orig($self, @args);
+    };
+
+=cut
+
