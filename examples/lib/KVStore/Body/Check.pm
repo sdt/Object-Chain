5f0ac6add99bee10a1ee3852aa1d279a5540fcbb
Stephen Thirlwall <sdt@dr.com>
2011-12-27 16:01:04 +1100

    Sample implementation for auto-delegated methods

--- examples/lib/KVStore/Body/Check.pm
+++ examples/lib/KVStore/Body/Check.pm
@@ -1,14 +1,34 @@
 package KVStore::Body::Check;
 
+use feature 'say';
+
 use Moose;
 use namespace::autoclean;
 
 use KVStore;
 
 # Can't do this(?) - chicken and egg problem
-#has +tail => (
-#    handles => 'get',
+#has '+tail' => (
+    #handles => 'get',
 #);
+
+{
+    package MC::Util;
+
+    # This is a workaround for has '+tail' => ( handles =>  ... );
+    sub tail_handles {
+        my $class = shift;
+        my $caller = scalar caller;
+        for my $method_name (@_) {
+            $caller->meta->add_method($method_name, sub {
+                    my $self = shift;
+                    return $self->tail->$method_name(@_);
+                });
+        }
+    }
+}
+
+MC::Util->tail_handles qw( get );
 
 with 'KVStore::Role::Body';
 
@@ -19,7 +39,4 @@ sub set {
     return;
 }
 
-sub get {
-    my ($self, $head, $key) = @_;
-    return $self->tail->get($head, $key);
-}
+1;
